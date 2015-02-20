47d79a137b4b4661b282a73c3657bf7a00f6053f
pat <fattalist@hotmail.com>
2015-02-19 17:06:36 -0700

    started scene

---
+++ Assets/Refrence assets/RunningDemo/MoveForward.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForward : MonoBehaviour {
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		player.transform.Translate (Vector3.up*Time.deltaTime);
+	
+	}
+}
