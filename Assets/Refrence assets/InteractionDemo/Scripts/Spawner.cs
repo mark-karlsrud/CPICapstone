1ec5ca4ba272e778093744e5e462a2fa7c4b837e
pat <fattalist@hotmail.com>
2015-02-23 15:43:29 -0700

    Pat's grab scene update

---
+++ Assets/Refrence assets/InteractionDemo/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject ball, barrel;
+	public float waitTimer =3.0f;
+	public float waitTimer2 =5.0f;
+	Timer timer;
+	private bool goingUp;
+	
+	
+	void Start () 
+	{
+		//Instantiate (ball);
+		//transform.Translate (Vector3.up * Time.deltaTime);
+		timer = new Timer();
+		timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+		timer.Interval = 3000;
+		timer.Enabled = true;
+	}
+	
+	
+	void Update () 
+	{
+		if (goingUp) 
+		{
+			transform.Translate (Vector3.up * 0.1f);
+
+		} else 
+		{
+
+			transform.Translate (Vector3.down * 0.1f);
+		}
+
+	}
+	private void OnTimedEvent(object source, ElapsedEventArgs e)
+	{
+		goingUp = !goingUp;
+	
+		Debug.Log("timer event");
+		//timer.Enabled = false;
+	}
+}
+
---
+++ Assets/Refrence assets/InteractionDemo/Scripts/SphereSmarts.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereSmarts : MonoBehaviour {
+	public GameObject player;
+	public GameObject icoCollider;
+	public bool didCollide, run ,start;
+	
+	
+	void Start()
+	{
+		start = true;
+		rigidbody.velocity = Vector3.right * 5;
+		
+	}
+	// Update is called once per frame
+	
+	void Update()
+	{
+		if (start) {
+			//transform.Translate (new Vector3 (-1, 0, 0)*Time.deltaTime);	
+		}
+		
+		if (run) {
+			run = true;
+			//transform.Translate (new Vector3 (1, 0, 0)*Time.deltaTime);	
+		}
+	}
+	
+	void OnCollisionEnter (Collision other) {
+		if(other.gameObject.tag == "Player") {
+			didCollide = true;
+			run=true;
+		}
+	}
+}
