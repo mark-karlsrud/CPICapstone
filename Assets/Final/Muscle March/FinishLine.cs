b334e113d7cd9738f00ef49fccab1d6839a960e3
Mark <markkarlsrud@aol.com>
2015-04-28 18:05:07 -0700

    finishline script

---
+++ Assets/Final/Muscle March/FinishLine.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        col.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+        scaleAndRestart();
+    }
+
+    public IEnumerator scaleAndRestart()
+    {
+        yield return new WaitForSeconds(3);
+        Application.LoadLevel(Application.loadedLevel);
+
+    }
+}
